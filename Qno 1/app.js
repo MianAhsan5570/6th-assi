f511c558018e775f3208a265bd9102b096dd4576
mianahsan5570 <mianahsan5570@gmail.com>
2022-09-15 17:59:26 +0500

    6th ass

--- Qno 1/app.js
+++ Qno 1/app.js
@@ -1,7 +1,11 @@
 // ******************************Qno 1*************************************
 // Write a ts program to print all natural numbers from 1 to n. - using while loop
 var naturalNumber = 0;
+var n = 2000;
 while (true) {
     console.log(naturalNumber);
     naturalNumber++;
+    if (naturalNumber == n) {
+        break;
+    }
 }
